b189f6b5fe47173dfb3e185415f6aba58e8a3211
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2021-12-25 12:42:18 +0600

    all product page done

--- src/components/pages/AllProduct/AllProducts.js
+++ src/components/pages/AllProduct/AllProducts.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 import Header from "../../shared_components/Header/Header";
+import SingleProduct from "../Home/SingleProduct/SingleProduct";
+import "./AllProduct.css";
 
 const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/furnitures.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div>
       <Header />
-      <h1>This is all product</h1>
+      <Container className="product-container">
+        <h2 className="mt-5 uh" style={{ color: "#FF7004" }}>
+          OUR ALL PRODUCT
+        </h2>
+        <div className="underline mb-5"></div>
+        <Row lg={2} md={2} sm={2} xs={1} className="g-4">
+          {products.map((product) => (
+            <SingleProduct key={product.key} product={product}></SingleProduct>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
--- src/components/pages/Home/Products/Products.js
+++ src/components/pages/Home/Products/Products.js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import SingleProduct from "../SingleProduct/SingleProduct";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -15,14 +16,20 @@ const Products = () => {
   }, []);
   return (
     <Container className="product-container">
-      <h2 className="my-5" style={{ color: "#f85f13" }}>
+      <h2 className="mt-5 uh" style={{ color: "#FF7004" }}>
         POPULAR FURNITURE
       </h2>
+      <div className="underline mb-5"></div>
       <Row lg={2} md={2} sm={2} xs={1} className="g-4">
         {products.slice(0, 6).map((product) => (
           <SingleProduct key={product.key} product={product}></SingleProduct>
         ))}
       </Row>
+      <div className="text-center m-5">
+        <Link to="/all-products">
+          <button className="btn btn-warning">VIEW OUR STORE</button>
+        </Link>
+      </div>
     </Container>
   );
 };
