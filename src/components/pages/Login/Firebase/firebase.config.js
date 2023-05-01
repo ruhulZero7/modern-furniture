f85317e77bacc81f31ac5adf8d0df90a96ee1fe9
ruhulamin77 <ruhulaminbd93@gmail.com>
2023-05-01 18:27:27 +0600

    environment variable updated

--- src/components/pages/Login/Firebase/firebase.config.js
+++ src/components/pages/Login/Firebase/firebase.config.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIK_EY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
